7c7675bd1cd348d11bc1d1329fadfcd6f8f8dbc8
Danny Swayne <DannyS@wsrb.com>
2022-05-25 10:33:42 -0700

    adding state to toolbar

--- src/components/SpeakersToolbar.js
+++ src/components/SpeakersToolbar.js
@@ -1,32 +1,46 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const SpeakersToolbar = () => {
-  return (
-    <section className="toolbar dark-theme-header">
-        <div className="container">
-            <div className="justify-content-between">
-                <ul className="toolrow d-flex flex-column flex-lg-row">
-                    <li className="d-flex flex-column flex-md-row">
-                        <b>Show Sessions&nbsp;&nbsp;</b>
-                        <label className="fav">
-                            <input type="checkbox" checked={true} />
-                        <span className="switch"></span>
-                        </label>
-                    </li>
-                    <li className="d-flex flex-column flex-md-row ml-sm-5 ml-0">
-                        <strong>Theme</strong>
-                        <label className="dropdown">
-                            <select className="form-control theme" value="light">
-                                <option value="light">Light</option>
-                                <option value="dark">Dark</option>
-                            </select>
-                        </label>
-                    </li>
-                </ul>
+
+    const [showSessions, setShowSessions] = useState(true);
+    const [theme, setTheme] = useState("light");
+
+    return (
+        <section className="toolbar dark-theme-header">
+            <div className="container">
+                <div className="justify-content-between">
+                    <ul className="toolrow d-flex flex-column flex-lg-row">
+                        <li className="d-flex flex-column flex-md-row">
+                            <b>Show Sessions&nbsp;&nbsp;</b>
+                            <label className="fav">
+                                <input 
+                                    type="checkbox" 
+                                    checked={showSessions}
+                                    onChange={(e) => {
+                                        setShowSessions(e.target.checked);
+                                    }}    
+                                />
+                                <span className="switch"></span>
+                            </label>
+                        </li>
+                        <li className="d-flex flex-column flex-md-row ml-sm-5 ml-0">
+                            <strong>Theme</strong>
+                            <label className="dropdown">
+                                <select 
+                                    className="form-control theme" 
+                                    value={theme}
+                                    onChange={(e) => setTheme(e.target.value)}
+                                >
+                                    <option value="light">Light</option>
+                                    <option value="dark">Dark</option>
+                                </select>
+                            </label>
+                        </li>
+                    </ul>
+                </div>
             </div>
-        </div>
-    </section>
-  )
+        </section>
+    )
 }
 
 export default SpeakersToolbar;
