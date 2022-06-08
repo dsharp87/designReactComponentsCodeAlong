d9551ebe6862f741ebd742e0df0e94c3453061b8
Danny Swayne <DannyS@wsrb.com>
2022-06-08 16:01:40 -0700

    now with error handling and loading skeleton

--- src/components/SpeakersList.js
+++ src/components/SpeakersList.js
@@ -1,11 +1,34 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 
+import ReactPlaceHolder from 'react-placeholder';
 import Speaker from './Speaker'
 import {data} from '../../SpeakerData';
 
 const SpeakersList = ({showSessions}) => {
 
-  const [speakersData, setSpeakersData] = useState(data);
+  const [speakersData, setSpeakersData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [hasErrored, setHasErrored] = useState(false);
+  const [error, setError] = useState("");
+
+  const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+  useEffect(() => {
+
+    async function delayFunc() {
+      try {
+        await delay(2000);
+        //throw "YEEAAAARRRGGGGGG";
+        setIsLoading(false);
+        setSpeakersData(data);
+      } catch (e) {
+        setIsLoading(false);
+        setHasErrored(true);
+        setError(e);
+      }
+    }
+    delayFunc();
+  },[]);
 
   const onfavoriteToggle = (id) => {
     const speakersRecPrevious = speakersData.find((speaker) => speaker.id === id);
@@ -17,8 +40,23 @@ const SpeakersList = ({showSessions}) => {
     setSpeakersData(speakersDataNew);
   };
 
+  if(hasErrored)
+    return (
+      <div className="text-danger">
+        ERROR: <b>leading Speaker Data Failed: {error}</b>
+      </div>
+    )
+
+  //if (isLoading) return <div>Loading...</div>
+
   return (
     <div className="container speakers-list">
+      <ReactPlaceHolder
+        type="media"
+        rows={15}
+        className="speakerslist-placeholder"
+        ready={isLoading === false}
+      >
         <div className="row">
             {speakersData.map(function (speaker) {
                 return (
@@ -31,6 +69,7 @@ const SpeakersList = ({showSessions}) => {
                 )
             })}
         </div>
+      </ReactPlaceHolder>
     </div>  
   )
 }
